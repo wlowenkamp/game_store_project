ce85722bbb4ebc9fe2af4db727ad087340df382d
Will Lowenkamp <lowenkamp97@gmail.com>
2023-07-18 10:07:56 -0400

    initial commit

---
+++ lib/game_store.py
@@ -0,0 +1,4 @@
+class Game_Store:
+    
+    def __init__( self, name ):
+        self.name = name
---
+++ lib/games.py
@@ -0,0 +1,14 @@
+class Game:
+    def __init__(self, title, genre, price):
+        self.title = title
+        self.genre = genre 
+        self.price = price
+    
+    def display_details(self):
+        print(f"Title: {self.title}")
+        print(f"Genre: {self.genre}")
+        print(f"Price: ${self.price}")
+
+game1 = Game("Super Smash Brothers Brawl", "Fighting", 67.50)
+
+print(game1)
