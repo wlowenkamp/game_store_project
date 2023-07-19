1515a1293ff8a56106df339f1e7e54825b546d80
Will Lowenkamp <lowenkamp97@gmail.com>
2023-07-19 12:18:05 -0400

    changes were made

---
+++ lib/console.py
@@ -0,0 +1,9 @@
+class Console:
+    def __init__(self, name):
+        self.name = name
+
+consoles = [
+    Console("Xbox"),
+    Console("PlayStation"),
+    Console("Nintendo"),
+]
---
+++ lib/genre.py
@@ -0,0 +1,10 @@
+class Genre:
+    def __init__(self, name):
+        self.name = name
+
+genres = [
+    Genre("Platform"),
+    Genre("Fighting"),
+    Genre("First Person Shooter"),
+    Genre("Action-Adventure"),
+]
